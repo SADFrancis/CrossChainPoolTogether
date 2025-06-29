fcb5d06dec648140c7ed0c4f529418a020f91da8
zbcoding <zbcoding>
2025-06-28 19:55:05 -0500

    edit DeploySender script with authorizedCallers parameter

--- scripts/DeploySender.s.sol
+++ scripts/DeploySender.s.sol
@@ -1,33 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
-import "forge-std/Script.sol";
-import {Sender} from "../src/ccip-usdc-example/USDCSender.sol";
 
+import "forge-std/Script.sol";
+import { Sender } from "../src/ccip-usdc-example/USDCSender.sol";
 
 // deploying this to avalanche Fuji
 // source .env
-// forge script ./script/DeployTransferUSDC.s.sol:DeployTransferUSDC -vvv --broadcast --rpc-url avalancheFuji
+// forge script ./scripts/DeploySender.s.sol:DeploySender --vvv --rpc-url avalancheFuji --broadcast
 
 contract DeploySender is Script {
+    // Avalanche Fuji Addresses
+    address router = 0xF694E193200268f9a4868e4Aa017A0118C9a8177;
+    address link = 0x0b9d5D9136855f6FEc3c0993feE6E9CE8a297846;
+    address usdcToken = 0x5425890298aed601595a70AB815c96711a31B684;
+
     function run() public {
-        //uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        vm.startBroadcast();
+        uint256 deployerPrivateKey = vm.envUint("HDWALLET_PRIVATE_KEY");
+        if (deployerPrivateKey == 0) {
+            revert("HDWALLET_PRIVATE_KEY must be set in your .env or .envrc file");
+        }
+        address deployerAddress = vm.addr(deployerPrivateKey);
 
-    /// @notice Constructor initializes the contract with the router address.
-    /// @param _router The address of the router contract.
-    /// @param _link The address of the link contract.
-    /// @param _usdcToken The address of the usdc contract.
-        // Sender sender = new Sender(
-        //    // /*address*/ _router, 
-        //    // /*address*/ _link,
-        //   //  /*address*/ _usdcToken
-        //     );
+        vm.startBroadcast(deployerPrivateKey);
 
-        // console.log(
-        //     "Sender contract deployed to ",
-        //     address(sender)
-        // );
+        // The deployer of the contract will be the owner and can add/remove callers later.
+
+        address[] memory authorizedCallers = new address[](3);
+        authorizedCallers[0] = deployerAddress; // The deployer is an authorized caller.
+        authorizedCallers[1] = 0xe865658aF136ffcF2D12BE81ED825239FF295A6D;
+        authorizedCallers[2] = 0x82E0Cd516dB8f4fa09bE0083aa11Bf9C80Ef3eA0;
+
+        Sender sender = new Sender(router, link, usdcToken, authorizedCallers);
+
+        console.log("Sender contract deployed to ", address(sender));
 
         vm.stopBroadcast();
     }
 }
+
+
