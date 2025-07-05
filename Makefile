24ef0117c39176133e368db671e327224645e3fe
SADFrancis <SADFrancis@gmail.com>
2025-07-04 23:40:59 -0400

    Trying to fix approval crosschain transaction

--- Makefile
+++ Makefile
@@ -12,7 +12,9 @@
 # https://dev.pooltogether.com/protocol/deployments/testnets/base-sepolia/
 
 #  Sender Contract Avalanche Fuji
-    # https://testnet.snowtrace.io/address/0x076a964f4D318F08a594435a6e0421B381c2eb32
+    # https://testnet.snowtrace.io/address/0x73faf5b8b4c13f9333b304694e909dff908ebe26
+
+	
 
     # // USDC PrizePool contract Base Sepolia
     # 0xa8322fd822ad303181CB29C0125ef137179b6658
@@ -108,8 +110,8 @@ fund-usdc:
 	cast send $(SOURCE_USDC) "transfer(address,uint256)" $(DEPLOYED_SOURCE_USDC_SENDER) 1000000 --rpc-url $(SOURCE_RPC_URL)
 
 # Send Link (18 decimals) to Sender contract: default 1 
-# to send 30 see command below:
-# $ make fund-link AMOUNT=30
+# to send 70 see command below:
+# $ make fund-link AMOUNT=70
 fund-link:
 	cast send $(SOURCE_LINK) "transfer(address,uint256)" $(DEPLOYED_SOURCE_USDC_SENDER) $(AMOUNT)ether --rpc-url $(SOURCE_RPC_URL)
 
@@ -181,7 +183,7 @@ vault-get-total-assets:
 # Cross chain approve
 
 cross-chain-usdc-approve:
-	cast send $(DEPLOYED_SOURCE_USDC_SENDER) "crossChainApprovalPayLink(uint64,address,uint256)" $(DESTINATION_CHAIN_SELECTOR) $(DESTINATION_ROUTER) $(USDC_AMOUNT) --rpc-url $(SOURCE_RPC_URL) 
+	cast send $(DEPLOYED_SOURCE_USDC_SENDER) "crossChainApproval(uint64,address,uint256,bool)" $(DESTINATION_CHAIN_SELECTOR) $(DEPLOYED_DESTINATION_RECEIVER) $(USDC_AMOUNT) true --rpc-url $(SOURCE_RPC_URL) 
 
 # Cross Chain deposit USDC into the Prize Vault AND PAY LINK!!!
 cross-chain-usdc-deposit:
--- src/ccip-usdc-example/USDCSender.sol
+++ src/ccip-usdc-example/USDCSender.sol
@@ -57,11 +57,15 @@ contract Sender is OwnerIsCreator {
     IRouterClient private immutable i_router;
     IERC20 private immutable i_linkToken;
     IERC20 private immutable i_usdcToken;
+    //Client.EVMTokenTokenAmount zeroTokenAmount = Client.EVMTokenAmount[](0);
 
     // Mapping to keep track of the receiver contract per destination chain.
     mapping(uint64 => address) public s_receivers;
     // Mapping to store the gas limit per destination chain.
     mapping(uint64 => uint256) public s_gasLimits;
+
+    // Mapping to store addresses to pay in Link (true) or native gas token (false)
+    mapping(bool => address) public i_payInLinkOrGasToken;
 
     modifier validateDestinationChain(uint64 _destinationChainSelector) {
         if (_destinationChainSelector == 0) revert InvalidDestinationChain();
@@ -79,6 +83,8 @@ contract Sender is OwnerIsCreator {
         i_router = IRouterClient(_router);
         i_linkToken = IERC20(_link);
         i_usdcToken = IERC20(_usdcToken);
+        i_payInLinkOrGasToken[true] = _link;
+        i_payInLinkOrGasToken[false] = address(0);
     }
 
     /// @dev Set the receiver contract for a given destination chain.
@@ -234,61 +240,72 @@ contract Sender is OwnerIsCreator {
         i_usdcToken.safeTransfer(_beneficiary, amount);
     }
 
-        /**
+    /**
      * @notice Approve `_staker` to spend `_amount` USDC on the destination chain.
      *         Pays CCIP fees in LINK held by this contract.
      *
-     * @param _destChain  CCIP chain selector of the destination chain.
-     * @param _staker     Address that should receive the allowance on dest-chain.
+     * @param _destinationChainSelector  CCIP chain selector of the destination chain.
+     * @param _addressToApprove     Address that should receive the allowance on dest-chain.
      * @param _amount     Allowance amount in USDC’s smallest unit (6 decimals).
-     *
-     * @return msgId      The CCIP message ID.
+     * @param _payGasInLink   Bool to choose to between Link or Gas Token as payment
+     * @return messageId      The CCIP message ID.
      */
-    function crossChainApprovalPayLink(
-        uint64  _destChain,
-        address _staker,
-        uint256 _amount
+    function crossChainApproval(
+        uint64  _destinationChainSelector,
+        address _addressToApprove,
+        uint256 _amount,
+        bool _payGasInLink
     )
         external
         onlyOwner
-        validateDestinationChain(_destChain)
-        returns (bytes32 msgId)
+        validateDestinationChain(_destinationChainSelector)
+        returns (bytes32 messageId)
     {
         /* ──────────────────────── sanity checks ───────────────────────── */
-        if (_staker == address(0))      revert InvalidReceiverAddress();
+        if (_addressToApprove == address(0))      revert InvalidReceiverAddress();
         if (_amount == 0)               revert AmountIsZero();
 
-        address receiver = s_receivers[_destChain];
-        if (receiver == address(0))     revert NoReceiverOnDestinationChain(_destChain);
+        address receiver = s_receivers[_destinationChainSelector];
+        if (receiver == address(0))     revert NoReceiverOnDestinationChain(_destinationChainSelector);
 
-        uint256 gasLimit = s_gasLimits[_destChain];
-        if (gasLimit == 0)              revert NoGasLimitOnDestinationChain(_destChain);
+        uint256 gasLimit = s_gasLimits[_destinationChainSelector];
+        if (gasLimit == 0)              revert NoGasLimitOnDestinationChain(_destinationChainSelector);
+
+        address gasToken = i_payInLinkOrGasToken[_payGasInLink];
 
         /* ─────────────────────── build CCIP payload ───────────────────── */
 
         // Call data: usdc.approve( _staker, _amount )
         bytes memory callData = abi.encodeWithSelector(
-            IERC20.approve.selector,
-            _staker,
+            i_usdcToken.approve.selector,
+            _addressToApprove,
             _amount
         );
 
-        Client.EVM2AnyMessage memory msg_ = Client.EVM2AnyMessage({
-            receiver:    abi.encode(receiver),  // usdc token on dest-chain
-            data:        callData,
-            tokenAmounts: new Client.EVMTokenAmount[](0) , // ❌ no token transfer
-            extraArgs:   Client._argsToBytes(
-                Client.GenericExtraArgsV2({
-                    gasLimit: gasLimit,
-                    allowOutOfOrderExecution: true
-                })
-            ),
-            feeToken: address(i_linkToken)               // pay LINK (Pay LINK mode)
-        });
+        Client.EVM2AnyMessage memory msg_ = _buildCCIPMessage(
+            _addressToApprove,
+             callData,
+            gasToken,
+            gasLimit,
+            true
+        );
+        
+        // Client.EVM2AnyMessage({
+        //     receiver:    abi.encode(receiver),  // usdc token on dest-chain
+        //     data:        callData,
+        //     tokenAmounts: new Client.EVMTokenAmount[](0) , // ❌ no token transfer
+        //     extraArgs:   Client._argsToBytes(
+        //         Client.GenericExtraArgsV2({
+        //             gasLimit: gasLimit,
+        //             allowOutOfOrderExecution: true
+        //         })
+        //     ),
+        //     feeToken: i_payInLinkOrGasToken[_payGasInLink]              // pay LINK (Pay LINK mode)
+        // });
 
         /* ───────────────────── fees & approvals ───────────────────────── */
 
-        uint256 fee = i_router.getFee(_destChain, msg_);
+        uint256 fee = i_router.getFee(_destinationChainSelector, msg_);
         if (fee > i_linkToken.balanceOf(address(this)))
             revert NotEnoughBalance(i_linkToken.balanceOf(address(this)), fee);
 
@@ -296,21 +313,19 @@ contract Sender is OwnerIsCreator {
 
         /* ───────────────────── send the message ───────────────────────── */
 
-        msgId = i_router.ccipSend(_destChain, msg_);
+        messageId = i_router.ccipSend(_destinationChainSelector, msg_);
 
         emit MessageSent(
-            msgId,
-            _destChain,
+            messageId,
+            _destinationChainSelector,
             receiver,
-            _staker,                 // beneficiary is the spender we just approved
+            _addressToApprove,                 // beneficiary is the spender we just approved
             address(i_usdcToken),
             _amount,
-            address(i_linkToken),              // fee paid in LINK
+            gasToken,              // fee paid in LINK
             fee
         );
     }
-
-
 
     /// @notice Sends data and transfer tokens to receiver on the destination chain.
     /// @notice Pay for fees in LINK.
@@ -336,37 +351,23 @@ contract Sender is OwnerIsCreator {
         uint256 gasLimit = s_gasLimits[_destinationChainSelector];
         if (gasLimit == 0)
             revert NoGasLimitOnDestinationChain(_destinationChainSelector);
+        
+        
         // Create an EVM2AnyMessage struct in memory with necessary information for sending a cross-chain message
-        // address(linkToken) means fees are paid in LINK
-        Client.EVMTokenAmount[]
-            memory tokenAmounts = new Client.EVMTokenAmount[](1);
-        tokenAmounts[0] = Client.EVMTokenAmount({
-            token: address(i_usdcToken),
-            amount: _amount
-        });
-        // Create an EVM2AnyMessage struct in memory with necessary information for sending a cross-chain message
-        Client.EVM2AnyMessage memory evm2AnyMessage = Client.EVM2AnyMessage({
-            receiver: abi.encode(receiver), // ABI-encoded receiver address
-            data: abi.encodeWithSelector(
-                IPrizeVault.deposit.selector, //@audit-info  CHANGE FOR PRIZE POOL
-                _amount,
-                _beneficiary
-                
-            ), // Encode the function selector and the arguments of the stake function
-            tokenAmounts: tokenAmounts, // The amount and type of token being transferred
-            extraArgs: Client._argsToBytes(
-                // Additional arguments, setting gas limit and allowing out-of-order execution.
-                // Best Practice: For simplicity, the values are hardcoded. It is advisable to use a more dynamic approach
-                // where you set the extra arguments off-chain. This allows adaptation depending on the lanes, messages,
-                // and ensures compatibility with future CCIP upgrades. Read more about it here: https://docs.chain.link/ccip/concepts/best-practices/evm#using-extraargs
-                Client.GenericExtraArgsV2({
-                    gasLimit: gasLimit, // Gas limit for the callback on the destination chain
-                    allowOutOfOrderExecution: true // Allows the message to be executed out of order relative to other messages from the same sender
-                })
-            ),
-            // Set the feeToken to a feeTokenAddress, indicating specific asset will be used for fees
-            feeToken: address(i_linkToken)
-        });
+        Client.EVM2AnyMessage memory evm2AnyMessage 
+            = _buildCCIPMessage(
+                _beneficiary,
+                abi.encodeWithSelector(
+                    IStaker.stake.selector, //@audit-info  CHANGE FOR PRIZE POOL
+                    _beneficiary,
+                    _amount
+                ), // Encode the function selector and the arguments of the stake function),
+                i_payInLinkOrGasToken[true], // paying in Link
+                gasLimit,
+                true,
+                address(i_usdcToken),
+                _amount
+        );
 
         // Get the fee required to send the CCIP message
         uint256 fees = i_router.getFee(
@@ -400,8 +401,76 @@ contract Sender is OwnerIsCreator {
             address(i_linkToken),
             fees
         );
-
         // Return the message ID
         return messageId;
+    }
+    function _buildCCIPMessage(
+        address _receiver,
+        bytes memory _data,
+        address _feeTokenAddress,
+        uint256 _gasLimit,
+        bool _allowOutOfOrderExecution
+
+    ) private pure returns (Client.EVM2AnyMessage memory){
+
+        // setting token amounts:
+        // If not sending any token, create an empty array, if yes, create 1 size array
+        Client.EVMTokenAmount[]
+            memory tokenAmounts = new Client.EVMTokenAmount[](0);
+
+        // Create EVM2AnyMessage struct in memorry with necessary info for sending cross chain message
+        return
+        Client.EVM2AnyMessage({
+            receiver: abi.encode(_receiver), // ABI-encoded receiver address
+            data: _data, // abi.encode(_text) -> BI-encoded string
+            tokenAmounts: tokenAmounts,
+            extraArgs: Client._argsToBytes(
+                Client.GenericExtraArgsV2({
+                    gasLimit: _gasLimit, // 200_000 -  gas limit for callback on destination chain
+                    allowOutOfOrderExecution: _allowOutOfOrderExecution // true
+                })
+            ),
+            feeToken: _feeTokenAddress
+        });
+    }
+
+
+    function _buildCCIPMessage( // I can use the same function name with extra parameters and it'll recognize as unique
+        address _receiver,
+        bytes memory _data,
+        address _feeTokenAddress,
+        uint256 _gasLimit,
+        bool _allowOutOfOrderExecution,
+        address _token,
+        uint256 _amount
+        ) private pure returns (Client.EVM2AnyMessage memory){
+
+        // setting token amounts:
+        // If not sending any token, create an empty array, if yes, create 1 size array
+        Client.EVMTokenAmount[]
+            memory tokenAmounts = _token == address(0) || _amount == 0 ? 
+                new Client.EVMTokenAmount[](0)
+            :   new Client.EVMTokenAmount[](1);
+
+       if (tokenAmounts.length > 0){
+            tokenAmounts[0] = Client.EVMTokenAmount({
+                token: _token,
+                amount: _amount
+            });
+        }
+        // Create EVM2AnyMessage struct in memorry with necessary info for sending cross chain message
+        return
+        Client.EVM2AnyMessage({
+            receiver: abi.encode(_receiver), // ABI-encoded receiver address
+            data: _data, // abi.encode(_text) -> BI-encoded string
+            tokenAmounts: tokenAmounts,
+            extraArgs: Client._argsToBytes(
+                Client.GenericExtraArgsV2({
+                    gasLimit: _gasLimit, // 200_000 -  gas limit for callback on destination chain
+                    allowOutOfOrderExecution: _allowOutOfOrderExecution // true
+                })
+            ),
+            feeToken: _feeTokenAddress
+        });
     }
 }
